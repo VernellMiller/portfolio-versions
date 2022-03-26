484bb486597c85fdb639473307532a0c8472b9b2
vernellmiller <nezeey@gmail.com>
2022-03-26 19:29:57 -0400

    wip: adds list to nav

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,9 +1,47 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
     <>
-      <h1>Navigation</h1>
+      <div class="sidenav bg-light">
+        <header class="title">
+          <Link to="/">
+            <p>Onward</p> {/* TODO I want to put a logo here. */}
+          </Link>
+        </header>
+
+        <ul>
+          <li>
+            <p>Getting Started</p>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <p>About Me</p>
+            <ul>
+              <li>
+                <p>Early Life</p>
+              </li>
+              <li>
+                <p>Interest</p>
+              </li>
+              <li>
+                <p>Passion</p>
+              </li>
+            </ul>
+          </li>
+        </ul>
+
+        <ul>
+          <li>Projects</li>
+        </ul>
+
+        <ul>
+          <li>Artwork</li>
+        </ul>
+      </div>
     </>
   );
 };
