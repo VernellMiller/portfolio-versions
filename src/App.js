d01920c1c07d54c4ef5b862e5b9cae872b38ec7c
vernellmiller <nezeey@gmail.com>
2022-03-26 12:44:08 -0400

    imports navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Navigation from "./Components/Navigation";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navigation />
         <a
           className="App-link"
           href="https://reactjs.org"
