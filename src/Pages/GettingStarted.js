3495dc017ac7c6734d5c8df0c762a67af0153c8a
vernellmiller <nezeey@gmail.com>
2022-03-29 11:49:51 -0400

    adjusts classes

--- src/Pages/GettingStarted.js
+++ src/Pages/GettingStarted.js
@@ -5,9 +5,9 @@ const GettingStarted = () => {
     <>
       <div class="container border">
         <div class="row">
-          <div class="col-9 mt-5 ml-5">
+          <div class="col mt-5 ml-5">
             <h1>Getting Started</h1>
-            <div class="small-box">small</div>
+            <div class="splash-image col-9"></div>
           </div>
         </div>
       </div>
