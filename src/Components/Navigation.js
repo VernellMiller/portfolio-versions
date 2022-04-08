c0314c0911830ea5a1f824b53fc0fcb2beb5297a
vernellmiller <nezeey@gmail.com>
2022-04-08 13:15:43 -0400

    adds title and navlinks components

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,50 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Title from "./Title";
+import NavigationLinks from "./NavigationLinks";
 
 const Navigation = () => {
   return (
     <>
       <div class="sidenav bg-light">
-        <div class="title">
-          <Link to="/">
-            <p>Onward</p> {/* TODO I want to put a logo here. */}
-          </Link>
-        </div>
-
-        <ul>
-          <li>
-            <a>Getting Started</a>
-          </li>
-        </ul>
-
-        <ul>
-          <li>
-            <a>About Me</a>
-            <ul>
-              <li>
-                <a>Early Life</a>
-              </li>
-              <li>
-                <a>Interest</a>
-              </li>
-              <li>
-                <a>Passion</a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-
-        <ul>
-          <li>Projects</li>
-        </ul>
-
-        <ul>
-          <li>Artwork</li>
-        </ul>
-
-        <ul>
-          <li>Blogs</li>
-        </ul>
+        <Title />
+        <NavigationLinks />
       </div>
     </>
   );
