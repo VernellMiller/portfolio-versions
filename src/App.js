68b095b7d8c4fcf58812285cf99cc1c7566c3576
vernellmiller <nezeey@gmail.com>
2022-04-09 16:46:39 -0400

    removes imported main component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import "./App.css";
 import Navigation from "./Components/Navigation";
-import Main from "./Components/Main";
 import GettingStarted from "./Pages/GettingStarted";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <body className="App-body">
-        <Navigation />
-        <GettingStarted />
+    <>
+      <div className="App">
+        <body className="App-body">
+          <Navigation />
+          <GettingStarted />
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </body>
-    </div>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </body>
+      </div>
+    </>
   );
 }
 
