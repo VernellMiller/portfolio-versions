e0473c90c2d7e6aef15029879f8a4b0d9aad6ebd
vernellmiller <nezeey@gmail.com>
2022-04-09 14:26:34 -0400

    wip: refactoring nav

--- src/Components/NavigationLinks.js
+++ src/Components/NavigationLinks.js
@@ -3,116 +3,137 @@ import React from "react";
 const NavigationLinks = () => {
   return (
     <>
-      <div></div>
-      {/*<div class="navlinks">
-        <ul>
-          <li>
-            <a class="text-decoration-none fw-bold" href="about">
-              Getting Started
-            </a>
-          </li>
-        </ul>
-
-        <ul>
-          <li>
-            <a class="text-decoration-none fw-bold" href="about">
-              About Me
-            </a>
-            <div>
-              <ul>
-                <li>
-                  <a class="text-decoration-none" href="earlyLife">
-                    Early Life
-                  </a>
-                </li>
-                <li>
-                  <a class="text-decoration-none" href="career">
-                    Career
-                  </a>
-                </li>
-                <li>
-                  <a class="text-decoration-none" href="passions">
-                    Passions
-                  </a>
-                </li>
-                <li>
-                  <a class="text-decoration-none" href="interest">
-                    Interest
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li>
-            <a class="text-decoration-none fw-bold" href="about">
-              Development Projects
-            </a>
-            <div>
-              <ul>
-                <li>
-                  <a class="text-decoration-none" href="project1">
-                    Project 1
-                  </a>
-                </li>
-                <li>
-                  <a class="text-decoration-none" href="roject2">
-                    Project 2
-                  </a>
-                </li>
-                <li>
-                  <a class="text-decoration-none" href="project3">
-                    Project 3
-                  </a>
-                </li>
-                <li>
-                  <a class="text-decoration-none" href="project4">
-                    Project 4
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li>
-            <a class="text-decoration-none fw-bold" href="2d3d">
-              2D | 3D Projects
-            </a>
-            <div>
-              <ul>
-                <li>
-                  <a class="text-decoration-none" href="2d">
-                    2D Design
-                  </a>
-                </li>
-                <li>
-                  <a class="text-decoration-none" href="3D">
-                    3D Design
-                  </a>
-                </li>
-                <li>
-                  <a class="text-decoration-none" href="branding">
-                    Branding
-                  </a>
-                </li>
-                <li>
-                  <a class="text-decoration-none" href="logos">
-                    Logos
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li>
-            <a class="text-decoration-none fw-bold" href="blogs">
-              Blogs
-            </a>
-          </li>
-          <li>
-            <a class="text-decoration-none fw-bold" href="support">
-              Support
-            </a>
-          </li>
-        </ul>
-  </div>*/}
+      <a href="#" class="navbar-brand mt-5">
+        <div class="display-5 font-weight-bold">this.</div>
+      </a>
+      <ul class="navbar-nav d-flex flex-column mt-5 w-100">
+        <li class="nav-item w-100">
+          <a href="#" class="nav-link pl-4">
+            Getting Started
+          </a>
+        </li>
+        <li class="nav-item dropdown w-100">
+          <a
+            href="#"
+            class="nav-link dropdown-toggle pl-4"
+            id="navbarDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-expanded="false"
+          >
+            About Me
+          </a>
+          <ul class="dropdown-menu w-100" aria-expanded="false">
+            <li>
+              <a href="#" class="dropdown-item pl-4 p-2">
+                Early Life
+              </a>
+            </li>
+            <li>
+              <a href="#" class="dropdown-item pl-4 p-2">
+                Career
+              </a>
+            </li>
+            <li>
+              <a href="#" class="dropdown-item pl-4 p-2">
+                Passions
+              </a>
+            </li>
+            <li>
+              <a href="#" class="dropdown-item pl-4 p-2">
+                Interests
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown w-100">
+          <a
+            href="#"
+            class="nav-link dropdown-toggle pl-4"
+            id="navbarDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Development Projects
+          </a>
+          <ul class="dropdown-menu w-100" aria-expanded="false">
+            <li>
+              <a href="#" class="dropdown-item pl-4 p-2">
+                Project 1
+              </a>
+            </li>
+            <li>
+              <a href="#" class="dropdown-item pl-4 p-2">
+                Project 1
+              </a>
+            </li>
+            <li>
+              <a href="#" class="dropdown-item pl-4 p-2">
+                Project 3
+              </a>
+            </li>
+            <li>
+              <a href="#" class="dropdown-item pl-4 p-2">
+                Project 4
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown w-100">
+          <a
+            href="#"
+            class="nav-link dropdown-toggle pl-4"
+            id="navbarDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-expanded="false"
+          >
+            2D | 3D Projects
+          </a>
+          <ul class="dropdown-menu w-100" aria-expanded="false">
+            <li>
+              <a href="#" class="dropdown-item pl-4 p-2">
+                2D Design
+              </a>
+            </li>
+            <li>
+              <a href="#" class="dropdown-item pl-4 p-2">
+                3D Design
+              </a>
+            </li>
+            <li>
+              <a href="#" class="dropdown-item pl-4 p-2">
+                Branding
+              </a>
+            </li>
+            <li>
+              <a href="#" class="dropdown-item pl-4 p-2">
+                Logos
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown w-100">
+          <a
+            href="#"
+            class="nav-link dropdown-toggle pl-4"
+            id="navbarDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Support
+          </a>
+          <ul class="dropdown-menu w-100" aria-expanded="false">
+            <li>
+              <a href="#" class="dropdown-item pl-4 p-2">
+                Contact Me
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
     </>
   );
 };
