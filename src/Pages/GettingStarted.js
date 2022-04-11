5facb3f6eb0d55b2ea66f2c215cc577e03364ef2
vernellmiller <nezeey@gmail.com>
2022-04-11 11:05:03 -0400

    updates header

--- src/Pages/GettingStarted.js
+++ src/Pages/GettingStarted.js
@@ -7,7 +7,7 @@ const GettingStarted = () => {
         <div class="row">
           <div class="col mt-5 ml-5">
             <header>
-              <h1>Getting Started</h1>
+              <h1>Getting Started Page</h1>
             </header>
             <div class="splash-image col-9"></div>
           </div>
