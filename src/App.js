9e863f096ffd360db9af782fb58f5a21174316f9
vernellmiller <nezeey@gmail.com>
2022-04-11 15:22:48 -0400

    wip: refactors and creates paths

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+
 import Navigation from "./Components/Navigation";
 import GettingStarted from "./Pages/GettingStarted";
 import Blogs from "./Pages/Blogs";
 import Contact from "./Pages/Contact";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import AboutMe from "./Pages/AboutMe";
 
 function App() {
   return (
     <>
+      {/* <Router> */}
       <div className="App">
         <body className="App-body">
           <Navigation />
-          <GettingStarted />
-          <Blogs />
-          <Contact />
-
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Routes>
+            <Route path="/" element={<GettingStarted />} />
+            <Route path="about" element={<AboutMe />} />
+            <Route path="blogs" element={<Blogs />} />
+            <Route path="contact" element={<Contact />} />
+          </Routes>
         </body>
       </div>
+      {/* </Router> */}
     </>
   );
 }
