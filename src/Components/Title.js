3d1305fb8ce5e5cdaa92c37c50b2f7f899ba7298
vernellmiller <nezeey@gmail.com>
2022-04-11 16:57:31 -0400

    imports link

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom";
 
 const Title = () => {
   return (
-    <div class="title">
-      <a href="#" class="navbar-brand mt-2">
-        <div class="display-5 font-weight-bold">this.</div>
-      </a>
-    </div>
+    <Link to="/">
+      <div class="title">
+        <a href="#" class="navbar-brand mt-2">
+          <div class="display-5 font-weight-bold">this.</div>
+        </a>
+      </div>
+    </Link>
   );
 };
 
