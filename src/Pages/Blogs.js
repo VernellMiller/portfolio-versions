8827874dd000197275321527f6abe5af444a3f21
vernellmiller <nezeey@gmail.com>
2022-04-11 10:57:12 -0400

    adds base component blogs and contact page

---
+++ src/Pages/Blogs.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Blogs = () => {
+  return (
+    <>
+      <div class="container">
+        <div class="row">
+          <div class="col mt-5 ml-5">
+            <header>
+              <h1>Blogs Page</h1>
+            </header>
+            <div class="splash-image col-9"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Blogs;
---
+++ src/Pages/Contact.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <>
+      <div class="container">
+        <div class="row">
+          <div class="col mt-5 ml-5">
+            <header>
+              <h1>Contact Page</h1>
+            </header>
+            <div class="splash-image col-9"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
