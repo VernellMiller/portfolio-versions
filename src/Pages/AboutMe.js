7017a978d6efc53c0b37ac7313106ef58a324953
vernellmiller <nezeey@gmail.com>
2022-04-11 16:56:59 -0400

    adds about me psedo content

--- src/Pages/AboutMe.js
+++ src/Pages/AboutMe.js
@@ -2,9 +2,50 @@ import React from "react";
 
 const AboutMe = () => {
   return (
-    <div>
-      <h1>About Me Page</h1>
-    </div>
+    <>
+      <div class="container">
+        <div class="row">
+          <div class="col mt-5 ml-5">
+            <header>
+              <h1>About Me Page Page</h1>
+            </header>
+            <div class="splash-image col-9"></div>
+          </div>
+        </div>
+        <section class="content col-9 mt-5">
+          <h5>Early Life</h5>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            quis aut itaque alias deserunt inventore fugit laudantium quos?
+            Tenetur facilis quas iure quia architecto aspernatur aliquid
+            voluptatem ratione? Optio, rem. Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Voluptatem laboriosam, consequatur
+            error, aliquid praesentium id deleniti at totam rem accusantium iste
+            numquam vel libero fuga? Dolor optio fugit placeat nihil.
+          </p>
+          <h5>Career</h5>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            quis aut itaque alias deserunt inventore fugit laudantium quos?
+            Tenetur facilis quas iure quia architecto aspernatur aliquid
+            voluptatem ratione? Optio, rem. Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Voluptatem laboriosam, consequatur
+            error, aliquid praesentium id deleniti at totam rem accusantium iste
+            numquam vel libero fuga? Dolor optio fugit placeat nihil.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            quis aut itaque alias deserunt inventore fugit laudantium quos?
+            Tenetur facilis quas iure quia architecto aspernatur aliquid
+            voluptatem ratione? Optio, rem. Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Voluptatem laboriosam, consequatur
+            error, aliquid praesentium id deleniti at totam rem accusantium iste
+            numquam vel libero fuga? Dolor optio fugit placeat nihil.
+          </p>
+          <h5>Passions</h5>
+        </section>
+      </div>
+    </>
   );
 };
 
