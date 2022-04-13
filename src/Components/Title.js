dcf9bf6cfaa264e3eeb6c46393b4344677ce7a33
vernellmiller <nezeey@gmail.com>
2022-04-12 20:09:09 -0400

    adds navbar brand class

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Title = () => {
   return (
     <Link to="/">
-      <div class="title">
+      <div class="title navbar-brand mt-2">
         <a href="#" class="navbar-brand mt-2">
           <div class="display-5 font-weight-bold">this.</div>
         </a>
