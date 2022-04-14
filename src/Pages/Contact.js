b3790f45de16d5372bb6c15d2f3388134134d389
vernellmiller <nezeey@gmail.com>
2022-04-14 14:42:57 -0400

    adds email link

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -10,6 +10,14 @@ const Contact = () => {
               <h1>Contact Page</h1>
             </header>
             <div class="splash-image col-9"></div>
+            <a href="mailto:vernellmiller1@gmailcom">
+              <div class="infoCard rounded col-4 mt-5">
+                <div class="infoboximage d-inline-block">
+                  <img src="/images/gmail.png" alt="gmail" />
+                </div>
+                <div class="infoboxname d-inline-block bg-light">Email</div>
+              </div>
+            </a>
           </div>
         </div>
       </div>
