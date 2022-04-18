89e2ace84d557a028302afb1a9dd26c352d4d02c
vernellmiller <nezeey@gmail.com>
2022-04-18 13:07:54 -0400

    creates project pages

---
+++ src/Pages/Project1.js
@@ -0,0 +1,20 @@
+import react from "react";
+
+const Project1 = () => {
+  return (
+    <>
+      <div class="container">
+        <div class="row">
+          <div class="col mt-5 ml-5">
+            <header>
+              <h1>Project 1 Page</h1>
+            </header>
+            <div class="splash-image col-9"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Project1;
---
+++ src/Pages/Project2.js
@@ -0,0 +1,20 @@
+import react from "react";
+
+const Project2 = () => {
+  return (
+    <>
+      <div class="container">
+        <div class="row">
+          <div class="col mt-5 ml-5">
+            <header>
+              <h1>Project 2 Page</h1>
+            </header>
+            <div class="splash-image col-9"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Project2;
