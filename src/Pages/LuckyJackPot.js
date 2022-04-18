3aea0ad8f888a20e31830aec0dba547e9a001878
vernellmiller <nezeey@gmail.com>
2022-04-18 17:03:01 -0400

    adds instructions

--- src/Pages/LuckyJackPot.js
+++ src/Pages/LuckyJackPot.js
@@ -10,6 +10,28 @@ const LuckyJackPot = () => {
               <h1>Welcome to Lucky Jack Pot</h1>
             </header>
             <div class="splash-image col-9"></div>
+            <section class="content col-9 mt-5">
+              <h5>Lucky Jack Pot</h5>
+              <p>
+                Lucky Jack Pot is a daily try your luck guessing game. Think you
+                got what it takes? Try it out, have some fun and who knows,
+                maybe you'll be todays Lucky Jack Pot Winner.
+              </p>
+
+              <br></br>
+
+              <h5>How to Play</h5>
+
+              <sub>Step 1:</sub>
+
+              <br></br>
+
+              <p>
+                Lucky Jack Pot is made up of a 26x26 square grid. Only one game
+                is played per 24 hours. Each player may attempt to guess the
+                jackpot location a max 4 times per day.
+              </p>
+            </section>
           </div>
         </div>
       </div>
