a0867b146233987c9efd4e143b7148f153c7d7c2
vernellmiller <nezeey@gmail.com>
2022-04-18 15:34:33 -0400

    creates base lucky jack pot component

---
+++ src/Pages/LuckyJackPot.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const LuckyJackPot = () => {
+  return (
+    <>
+      <div class="container">
+        <div class="row">
+          <div class="col mt-5 ml-5">
+            <header>
+              <h1>Welcome to Lucky Jack Pot</h1>
+            </header>
+            <div class="splash-image col-9"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LuckyJackPot;
