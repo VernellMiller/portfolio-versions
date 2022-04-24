c201b6b44291121dd29864420b7d54612587a28d
vernellmiller <nezeey@gmail.com>
2022-04-24 16:01:52 -0400

    addss yard images and components

---
+++ src/Components/YardProject.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const YardProject = () => {
+  return (
+    <>
+      <section className="content col-9 mt-5">
+        <h5>The Yard Build</h5>
+        <img
+          className="img-fluid"
+          src="/images/yardProject1.jpg"
+          alt="yardProject1"
+        />
+        <h6 className="mt-5">Welocome to the YardProject</h6>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+          quis aut itaque alias deserunt inventore fugit laudantium quos?
+          Tenetur facilis quas iure quia architecto aspernatur aliquid
+          voluptatem ratione? Optio, rem. Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Voluptatem laboriosam, consequatur
+          error, aliquid praesentium id deleniti at totam rem accusantium iste
+          numquam vel libero fuga? Dolor optio fugit placeat nihil.
+        </p>
+        <h5>The Yard Build</h5>
+        <img
+          className="img-fluid"
+          src="/images/yardProject2.jpg"
+          alt="yardProject2"
+        />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+          quis aut itaque alias deserunt inventore fugit laudantium quos?
+          Tenetur facilis quas iure quia architecto aspernatur aliquid
+          voluptatem ratione? Optio, rem. Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Voluptatem laboriosam, consequatur
+          error, aliquid praesentium id deleniti at totam rem accusantium iste
+          numquam vel libero fuga? Dolor optio fugit placeat nihil.
+        </p>
+      </section>
+      {/* <div className="splash-image col-9"></div> */}
+    </>
+  );
+};
+
+export default YardProject;
