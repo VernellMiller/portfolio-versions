d14a00beba9ae92fe23179a8f890eb39c3faab38
vernellmiller <nezeey@gmail.com>
2022-04-24 16:02:47 -0400

    adds township image and component

---
+++ src/Components/TownShip.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const TownShip = () => {
+  return (
+    <>
+      <section className="content col-9 mt-5">
+        <h5>Little Living | Shopping TownShip</h5>
+        <img
+          className="img-fluid"
+          src="/images/littleLiving.jpg"
+          alt="littleLiving"
+        />
+        <h6 className="mt-5">Welocome to the TownShip</h6>
+
+        <p>
+          The TownShip is a concept idea inspired by Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Molestiae quis aut itaque alias deserunt
+          inventore fugit laudantium quos? Tenetur facilis quas iure quia
+          architecto aspernatur aliquid voluptatem ratione? Optio, rem. Lorem
+          ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem
+          laboriosam, consequatur error, aliquid praesentium id deleniti at
+          totam rem accusantium iste numquam vel libero fuga? Dolor optio fugit
+          placeat nihil.
+        </p>
+      </section>
+      {/* <div className="splash-image col-9"></div> */}
+    </>
+  );
+};
+
+export default TownShip;
