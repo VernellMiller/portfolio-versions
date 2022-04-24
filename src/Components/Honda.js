96a15658c436479b240614d06b7af266efe0cd5d
vernellmiller <nezeey@gmail.com>
2022-04-24 16:01:19 -0400

    adds honda image and component

---
+++ src/Components/Honda.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Honda = () => {
+  return (
+    <>
+      <section className="content col-9 mt-5">
+        <h5>Honda</h5>
+        <img className="img-fluid" src="/images/honda.jpg" alt="honda" />
+        <h6 className="mt-5">Welocome to the Honda</h6>
+
+        <p>
+          The Honda is a concept idea inspired by Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Molestiae quis aut itaque alias deserunt
+          inventore fugit laudantium quos? Tenetur facilis quas iure quia
+          architecto aspernatur aliquid voluptatem ratione? Optio, rem. Lorem
+          ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem
+          laboriosam, consequatur error, aliquid praesentium id deleniti at
+          totam rem accusantium iste numquam vel libero fuga? Dolor optio fugit
+          placeat nihil.
+        </p>
+      </section>
+      {/* <div className="splash-image col-9"></div> */}
+    </>
+  );
+};
+
+export default Honda;
