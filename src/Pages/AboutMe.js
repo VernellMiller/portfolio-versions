652a44519ee0cf174ec9b787b8f3d8d20022e7a8
vernellmiller <nezeey@gmail.com>
2022-04-27 11:50:51 -0400

    imports career component

--- src/Pages/AboutMe.js
+++ src/Pages/AboutMe.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Career from "../Components/Career";
 
 const AboutMe = () => {
   return (
@@ -21,7 +22,7 @@ const AboutMe = () => {
           <p className="mt-5">
             <strong>Vernell Miller</strong> to be exact. Thank you for visiting.
           </p>
-          <h5 className="mt-5">Early Life</h5>
+          <h3 className="mt-5">Early Life</h3>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
             quis aut itaque alias deserunt inventore fugit laudantium quos?
@@ -31,26 +32,10 @@ const AboutMe = () => {
             error, aliquid praesentium id deleniti at totam rem accusantium iste
             numquam vel libero fuga? Dolor optio fugit placeat nihil.
           </p>
-          <h5>Career</h5>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-            quis aut itaque alias deserunt inventore fugit laudantium quos?
-            Tenetur facilis quas iure quia architecto aspernatur aliquid
-            voluptatem ratione? Optio, rem. Lorem ipsum dolor sit amet,
-            consectetur adipisicing elit. Voluptatem laboriosam, consequatur
-            error, aliquid praesentium id deleniti at totam rem accusantium iste
-            numquam vel libero fuga? Dolor optio fugit placeat nihil.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-            quis aut itaque alias deserunt inventore fugit laudantium quos?
-            Tenetur facilis quas iure quia architecto aspernatur aliquid
-            voluptatem ratione? Optio, rem. Lorem ipsum dolor sit amet,
-            consectetur adipisicing elit. Voluptatem laboriosam, consequatur
-            error, aliquid praesentium id deleniti at totam rem accusantium iste
-            numquam vel libero fuga? Dolor optio fugit placeat nihil.
-          </p>
-          <h5>Passions</h5>
+
+          <h3>Career</h3>
+          <Career />
+          <h3>Passions</h3>
         </section>
       </div>
     </>
