59b7bfbbecb499b2ca41b6a481489c34713df6ee
vernellmiller <nezeey@gmail.com>
2022-04-27 11:50:23 -0400

    WIP: creates career component and styles

---
+++ src/Components/Career.js
@@ -0,0 +1,156 @@
+import React from "react";
+
+const Career = () => {
+  return (
+    <>
+      <section className="mt-4" id="career">
+        <div class="position-relative">
+          <div
+            class="position-absolute top-0 start-0 translate-middle"
+            id="circle"
+          ></div>
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                <h5
+                  className="position-absolute top-0 start-0 translate-middle"
+                  id="job"
+                >
+                  Freelance, Remote
+                </h5>
+              </div>
+            </div>
+          </div>
+          <div className="container" id="jobDescription">
+            <div className="row">
+              <div className="col">
+                <h6 className="mt-3">
+                  Web Developer | 2D 3D Designer, Jan. 2020-Present
+                </h6>
+                <p>
+                  Coordinating with individual and enterprise clients to provide
+                  design services by completing various projects involving 2D/3D
+                  design, graphic design, branding, visual effects, motion
+                  graphics, info graphics and more. Details include
+                  brainstorming, sketching and creating the logos along with the
+                  overall identity of a Start up Company.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="mt-4" id="career">
+        <div class="position-relative">
+          <div
+            class="position-absolute top-0 start-0 translate-middle"
+            id="circle"
+          ></div>
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                <h5
+                  className="position-absolute top-0 start-0 translate-middle"
+                  id="job2"
+                >
+                  Mountain View Group, Atlanta, GA
+                </h5>
+              </div>
+            </div>
+          </div>
+          <div className="container" id="jobDescription">
+            <div className="row">
+              <div className="col">
+                <h6 className="mt-3">Intern, Oct. 2019-Dec. 2019</h6>
+                <p>
+                  Supported MVG’s 3D design team on AR and VR projects. Worked
+                  in 3D software, Blender, Unity and ARKit 3. Also supported
+                  their production team with video editing using premiere pro.
+                  Built from scratch a AR application for a weapons defense
+                  company to use as a sales tool.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="mt-4" id="career">
+        <div class="position-relative">
+          <div
+            class="position-absolute top-0 start-0 translate-middle"
+            id="circle"
+          ></div>
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                <h5
+                  className="position-absolute top-0 start-0 translate-middle"
+                  id="job3"
+                >
+                  The Carter Projekt, Various locations
+                </h5>
+              </div>
+            </div>
+          </div>
+          <div className="container" id="jobDescription">
+            <div className="row">
+              <div className="col">
+                <h6 className="mt-3">Tour Manager, March. 2018-July. 2018</h6>
+                <p>
+                  Managed a 10-member band and oversaw all rehearsals and
+                  performances throughout a 27-day tour. Coordinated with venues
+                  to schedule the 13 performances within the 27 days across the
+                  Mid-East, and North Africa for the US Military.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="mt-4" id="career">
+        <div class="position-relative">
+          <div
+            class="position-absolute top-0 start-0 translate-middle"
+            id="circle"
+          ></div>
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                <h5
+                  className="position-absolute top-0 start-0 translate-middle"
+                  id="job4"
+                >
+                  Prosys, Alpharetta, GA
+                </h5>
+              </div>
+            </div>
+          </div>
+          <div
+            class="position-absolute top-100 start-0 translate-middle"
+            id="circle"
+          ></div>
+          <div className="container" id="jobDescription">
+            <div className="row">
+              <div className="col">
+                <h6 className="mt-3">
+                  Configuration Engineer, Jan. 2012-Jan. 2016
+                </h6>
+                <p>
+                  Managed a 10-member band and oversaw all rehearsals and
+                  performances throughout a 27-day tour. Coordinated with venues
+                  to schedule the 13 performances within the 27 days across the
+                  Mid-East, and North Africa for the US Military.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Career;
