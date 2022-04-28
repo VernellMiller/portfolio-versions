2ea019a32dccc43b8c692444ec6015abd4dd8c5e
vernellmiller <nezeey@gmail.com>
2022-04-28 13:00:28 -0400

    adds honda description

--- src/Components/Honda.js
+++ src/Components/Honda.js
@@ -3,23 +3,26 @@ import React from "react";
 const Honda = () => {
   return (
     <>
-      <section className="content col-9 mt-5">
+      <section className="content col-9 mt-5 border-bottom">
         <h5>Honda</h5>
         <img className="img-fluid" src="/images/honda.jpg" alt="honda" />
         <h6 className="mt-5">Welocome to the Honda</h6>
 
         <p>
-          The Honda is a concept idea inspired by Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Molestiae quis aut itaque alias deserunt
-          inventore fugit laudantium quos? Tenetur facilis quas iure quia
-          architecto aspernatur aliquid voluptatem ratione? Optio, rem. Lorem
-          ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem
-          laboriosam, consequatur error, aliquid praesentium id deleniti at
-          totam rem accusantium iste numquam vel libero fuga? Dolor optio fugit
-          placeat nihil.
+          This was a fun yet challenging design project. The original was
+          designed in 3D using Unreal Engine by 3D Artist Coreray. Coreray does
+          great work and I was inspired to try to capture the 3D renders and
+          bring it to life in the 2D space. I designed this using Adobe
+          Illustrator.
+        </p>
+        <p>
+          This project help bolster my pen tool skills along with my skills
+          using the perspective grid tool. Using both really brought life to
+          this 2D rendition and adding the shadows and highlights blends it all
+          come together smoothly. It was great practice for me and I enjoyed it
+          a ton.
         </p>
       </section>
-      {/* <div className="splash-image col-9"></div> */}
     </>
   );
 };
