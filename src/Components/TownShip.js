45b17bc941fce2e314e3f5cefc0c8577bb564f9e
vernellmiller <nezeey@gmail.com>
2022-04-28 10:21:48 -0400

    adds township description

--- src/Components/TownShip.js
+++ src/Components/TownShip.js
@@ -11,16 +11,22 @@ const TownShip = () => {
           alt="littleLiving"
         />
         <h6 className="mt-5">Welocome to the TownShip</h6>
-
         <p>
-          The TownShip is a concept idea inspired by Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Molestiae quis aut itaque alias deserunt
-          inventore fugit laudantium quos? Tenetur facilis quas iure quia
-          architecto aspernatur aliquid voluptatem ratione? Optio, rem. Lorem
-          ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem
-          laboriosam, consequatur error, aliquid praesentium id deleniti at
-          totam rem accusantium iste numquam vel libero fuga? Dolor optio fugit
-          placeat nihil.
+          The TownShip is a concept idea inspired by a post seen on
+          @a24illustration. My concept is a little town road that features four
+          separate parks at the towns center that make up one large theme park
+          styled park. Strategically placed along the road and alongside the
+          parks are the residential buildings, a combination of Apartments and
+          Condominiums. Scattered amongst the parks and residential buildings
+          are a bevy of shops, grocery, convenience, and department stores for
+          the residents to enjoy.
+        </p>
+        <p>
+          This design was created in Adobe Illustrator. I enjoy working in
+          Illustrator and this project gave me another opportunity to continue
+          practicing in Graphic Design. I do have further plans to take this
+          design further by adding more 2D designs. However, I would love to
+          build a 3D concept as well. To be continued ➡➡➡.
         </p>
       </section>
       {/* <div className="splash-image col-9"></div> */}
