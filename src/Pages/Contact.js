3bc92d825c0d8c63275986ae2351ed939401366a
vernellmiller <nezeey@gmail.com>
2022-04-29 19:55:27 -0400

    creates working email form

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,23 +1,46 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_pdgfl5t",
+        "template_wofh7lu",
+        form.current,
+        "h4jiXDwtohJAXgJKq"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+
   return (
     <>
       <div className="container">
-        <div className="row">
-          <div className="col mt-5 ml-5">
-            <header>
-              <h1>Contact Page</h1>
-            </header>
-            <div className="splash-image col-9"></div>
-            <a href="mailto:vernellmiller1@gmailcom">
-              <div className="infoCard rounded col-4 mt-5">
-                <div className="infoboximage d-inline-block">
-                  <img src="/images/gmail.png" alt="gmail" />
-                </div>
-                <div className="infoboxname d-inline-block bg-light">Email</div>
-              </div>
-            </a>
+        <div className="content">
+          <div className="card">
+            <form ref={form} onSubmit={sendEmail}>
+              <label>to</label>
+              <input type="text" name="name" />
+              <label>from</label>
+              <input type="text" name="from" />
+              <label>Message</label>
+              <textarea name="message" />
+              <label>reply_to</label>
+              <input type="text" name="reply_to" />
+              <input type="submit" value="Send" />
+            </form>
           </div>
         </div>
       </div>
