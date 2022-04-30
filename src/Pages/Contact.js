65aef48caea8f8b92f09fdb7c7fdc4229a1cb73f
vernellmiller <nezeey@gmail.com>
2022-04-30 13:49:50 -0400

    wip: adds form, form image & styling

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -25,22 +25,49 @@ const Contact = () => {
     e.target.reset();
   };
 
+  const myName = "Vernell Miller";
+
   return (
     <>
-      <div className="container">
-        <div className="content">
-          <div className="card">
-            <form ref={form} onSubmit={sendEmail}>
-              <label>to</label>
-              <input type="text" name="name" />
-              <label>from</label>
-              <input type="text" name="from" />
-              <label>Message</label>
-              <textarea name="message" />
-              <label>reply_to</label>
-              <input type="text" name="reply_to" />
-              <input type="submit" value="Send" />
-            </form>
+      <div className="contactBgImage">
+        <div className="container mt-5">
+          <div className="content">
+            <div class="card mb-3 shadow" id="formCard">
+              <div className="card-body">
+                <form ref={form} onSubmit={sendEmail}>
+                  <p>to</p>
+                  <input
+                    type="text"
+                    className="col-12 border corder-light"
+                    name="to"
+                    value={myName}
+                  />
+                  <p>Your Name or Company</p>
+                  <input
+                    type="text"
+                    className="col-12 border corder-light"
+                    name="from"
+                  />
+                  <p>Message</p>
+                  <textarea
+                    className="col-12 border corder-light"
+                    name="message"
+                  />
+                  <p>Your Email or Phone #</p>
+                  <input
+                    type="text"
+                    className="col-12 border corder-light"
+                    name="reply_to"
+                  />
+                  <input
+                    type="submit"
+                    className="btn col-12 mt-3"
+                    value="Send Message"
+                  />
+                </form>
+              </div>
+              <div class="card-footer bg-transparent border">Footer</div>
+            </div>
           </div>
         </div>
       </div>
