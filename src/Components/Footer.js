59f9fd0de7fecd9bce50e288917812ea1b4a3e66
vernellmiller <nezeey@gmail.com>
2022-05-06 14:12:52 -0400

    removes h1

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return (
-    <div className="mt-5" id="footer">
-      <h1>Footer</h1>
-    </div>
-  );
+  return <div className="mt-5" id="footer"></div>;
 };
 
 export default Footer;
