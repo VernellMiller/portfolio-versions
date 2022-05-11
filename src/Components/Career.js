6466222176f4856501d5ef520419d2e49e354255
vernellmiller <nezeey@gmail.com>
2022-05-11 01:03:45 -0400

    adds examples to job descriptions

--- src/Components/Career.js
+++ src/Components/Career.js
@@ -3,152 +3,196 @@ import React from "react";
 const Career = () => {
   return (
     <>
-      <section className="mt-5" id="career">
-        <div className="position-relative">
-          <div
-            className="position-absolute top-0 start-0 translate-middle"
-            id="circle"
-          ></div>
-          <div className="container">
-            <div className="row">
-              <div className="col">
-                <h5
-                  className="position-absolute top-0 start-0 translate-middle"
-                  id="job"
-                >
-                  Freelance, Remote
-                </h5>
+      <div className="border-bottom">
+        <section className="mt-5" id="career">
+          <div className="position-relative">
+            <div
+              className="position-absolute top-0 start-0 translate-middle"
+              id="circle"
+            ></div>
+            <div className="container">
+              <div className="row">
+                <div className="col">
+                  <h5
+                    className="position-absolute top-0 start-0 translate-middle"
+                    id="job"
+                  >
+                    Freelance, Remote
+                  </h5>
+                </div>
+              </div>
+            </div>
+            <div className="container" id="jobDescription">
+              <div className="row">
+                <div className="col">
+                  <h6 className="mt-3">
+                    Web Developer | 2D 3D Designer, Jan. 2020-Present
+                  </h6>
+                  <p>
+                    Coordinating with individual and enterprise clients to
+                    provide design services by completing various projects
+                    involving 2D/3D design, graphic design, branding, visual
+                    effects, motion graphics, info graphics and more. Details
+                    include brainstorming, sketching and creating the logos
+                    along with the overall identity of a Start up Company.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-          <div className="container" id="jobDescription">
-            <div className="row">
-              <div className="col">
-                <h6 className="mt-3">
-                  Web Developer | 2D 3D Designer, Jan. 2020-Present
-                </h6>
-                <p>
-                  Coordinating with individual and enterprise clients to provide
-                  design services by completing various projects involving 2D/3D
-                  design, graphic design, branding, visual effects, motion
-                  graphics, info graphics and more. Details include
-                  brainstorming, sketching and creating the logos along with the
-                  overall identity of a Start up Company.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+        </section>
 
-      <section className="mt-4" id="career">
-        <div className="position-relative">
-          <div
-            className="position-absolute top-0 start-0 translate-middle"
-            id="circle"
-          ></div>
-          <div className="container">
-            <div className="row">
-              <div className="col">
-                <h5
-                  className="position-absolute top-0 start-0 translate-middle"
-                  id="job2"
-                >
-                  Mountain View Group, Atlanta, GA
-                </h5>
+        <section className="mt-4" id="career">
+          <div className="position-relative">
+            <div
+              className="position-absolute top-0 start-0 translate-middle"
+              id="circle"
+            ></div>
+            <div className="container">
+              <div className="row">
+                <div className="col">
+                  <h5
+                    className="position-absolute top-0 start-0 translate-middle"
+                    id="job2"
+                  >
+                    Mountain View Group, Atlanta, GA
+                  </h5>
+                </div>
+              </div>
+            </div>
+            <div className="container" id="jobDescription">
+              <div className="row">
+                <div className="col">
+                  <h6 className="mt-3">Intern, Oct. 2019-Dec. 2019</h6>
+                  <p>
+                    Supported MVG’s 3D design team on AR and VR projects. Worked
+                    in 3D software, Blender, Unity and ARKit 3. Also supported
+                    their production team with video editing using premiere pro.
+                    Built from scratch a AR application for a weapons defense
+                    company to use as a sales tool.
+                  </p>
+                  <p>
+                    Raytheon Augmented Reality Application Project. The scope of
+                    this project was to construct an innovative marketing asset
+                    through an Augmented Reality Application to present recently
+                    launched product devices to clientele.
+                  </p>
+                  <ul>
+                    <li>
+                      Met with business owners and stakeholders to construct
+                      design of asset.
+                    </li>
+                    <li>
+                      Consistently followed up with team members for action
+                      items to monitor & control the progress of milestones.
+                    </li>
+                    <li>
+                      Constructed 3D asset and script to deploy marketing
+                      Augmented Reality Application.
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
-          <div className="container" id="jobDescription">
-            <div className="row">
-              <div className="col">
-                <h6 className="mt-3">Intern, Oct. 2019-Dec. 2019</h6>
-                <p>
-                  Supported MVG’s 3D design team on AR and VR projects. Worked
-                  in 3D software, Blender, Unity and ARKit 3. Also supported
-                  their production team with video editing using premiere pro.
-                  Built from scratch a AR application for a weapons defense
-                  company to use as a sales tool.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+        </section>
 
-      <section className="mt-4" id="career">
-        <div className="position-relative">
-          <div
-            className="position-absolute top-0 start-0 translate-middle"
-            id="circle"
-          ></div>
-          <div className="container">
-            <div className="row">
-              <div className="col">
-                <h5
-                  className="position-absolute top-0 start-0 translate-middle"
-                  id="job3"
-                >
-                  The Carter Projekt, Various locations
-                </h5>
+        <section className="mt-4" id="career">
+          <div className="position-relative">
+            <div
+              className="position-absolute top-0 start-0 translate-middle"
+              id="circle"
+            ></div>
+            <div className="container">
+              <div className="row">
+                <div className="col">
+                  <h5
+                    className="position-absolute top-0 start-0 translate-middle"
+                    id="job3"
+                  >
+                    The Carter Projekt, Various locations
+                  </h5>
+                </div>
+              </div>
+            </div>
+            <div className="container" id="jobDescription">
+              <div className="row">
+                <div className="col">
+                  <h6 className="mt-3">Tour Manager, March. 2018-July. 2018</h6>
+                  <p>
+                    Managed a 10-member band and oversaw all rehearsals and
+                    performances throughout a 27-day tour. Coordinated with
+                    venues to schedule the 13 performances within the 27 days
+                    across the Mid-East, and North Africa for the US Military.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-          <div className="container" id="jobDescription">
-            <div className="row">
-              <div className="col">
-                <h6 className="mt-3">Tour Manager, March. 2018-July. 2018</h6>
-                <p>
-                  Managed a 10-member band and oversaw all rehearsals and
-                  performances throughout a 27-day tour. Coordinated with venues
-                  to schedule the 13 performances within the 27 days across the
-                  Mid-East, and North Africa for the US Military.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+        </section>
 
-      <section className="mt-4" id="career">
-        <div className="position-relative">
-          <div
-            className="position-absolute top-0 start-0 translate-middle"
-            id="circle"
-          ></div>
-          <div className="container">
-            <div className="row">
-              <div className="col">
-                <h5
-                  className="position-absolute top-0 start-0 translate-middle"
-                  id="job4"
-                >
-                  Prosys, Alpharetta, GA
-                </h5>
+        <section className="mt-4" id="career">
+          <div className="position-relative">
+            <div
+              className="position-absolute top-0 start-0 translate-middle"
+              id="circle"
+            ></div>
+            <div className="container">
+              <div className="row">
+                <div className="col">
+                  <h5
+                    className="position-absolute top-0 start-0 translate-middle"
+                    id="job4"
+                  >
+                    Prosys, Alpharetta, GA
+                  </h5>
+                </div>
+              </div>
+            </div>
+            <div
+              className="position-absolute top-100 start-0 translate-middle"
+              id="circle"
+            ></div>
+            <div className="container" id="jobDescription">
+              <div className="row">
+                <div className="col">
+                  <h6 className="mt-3">
+                    Configuration Engineer, Jan. 2012-Jan. 2016
+                  </h6>
+                  <p>
+                    Began working as a Configuration Technician and advanced to
+                    Co- Project Lead on the organization’s largest project with
+                    AT&T. Built server racks from scratch that contributed to
+                    several AT&T Data Centers across the country.
+                  </p>
+                  <p>
+                    AT&T Data Center Installation Project The purpose of this
+                    project is to provide an architecture solution,
+                    construction, delivery and installation of server racks &
+                    equipment for multiple AT&T Data Center locations across the
+                    US.
+                  </p>
+                  <ul>
+                    <li>
+                      Collaborated with technical team & stakeholders to
+                      identify scope, deliverables & milestone timelines.
+                    </li>
+                    <li>
+                      Coordinated Audit efforts to ensure quality control with
+                      project documentation.
+                    </li>
+                    <li>
+                      Lead team of 15-20 over 2 years to identify gaps and
+                      construct engineering architecture solutions to streamline
+                      supply chain & delivery
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
-          <div
-            className="position-absolute top-100 start-0 translate-middle"
-            id="circle"
-          ></div>
-          <div className="container" id="jobDescription">
-            <div className="row">
-              <div className="col">
-                <h6 className="mt-3">
-                  Configuration Engineer, Jan. 2012-Jan. 2016
-                </h6>
-                <p>
-                  Began working as a Configuration Technician and advanced to
-                  Co- Project Lead on the organization’s largest project with
-                  AT&T. Built server racks from scratch that contributed to
-                  several AT&T Data Centers across the country.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </>
   );
 };
