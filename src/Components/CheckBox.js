e2e1959eaf363428bb865d86316347b13dd35306
vernellmiller <nezeey@gmail.com>
2022-05-20 18:03:05 -0400

    adds seperate state

--- src/Components/CheckBox.js
+++ src/Components/CheckBox.js
@@ -1,22 +1,39 @@
 import React, { useState } from "react";
 
 const CheckBox = (props) => {
-  const [selections, setSelections] = useState({
-    selections: [],
-  });
+  // const [selections, setSelections] = useState({
+  //   selections: [],
+  // });
+
+  const [selection1, setSelection1] = useState("");
+  const [selection2, setSelection2] = useState("");
+  const [selection3, setSelection3] = useState("");
+  const [selection4, setSelection4] = useState("");
+
+  const selections = [];
 
   const getSelectionHandler = (event) => {
     const value =
       event.target.type === "checkbox"
         ? event.target.checked
         : event.target.value;
-
     console.log(event.target.value, value);
+    // setSelections((prevState) => {
+    //   return { ...prevState, selections: event.target.value };
+    // });
 
-    setSelections((prevState) => {
-      return { ...prevState, selections: event.target.value };
-    });
+    if (selection1 == "") {
+      setSelection1(event.target.value);
+    } else if (selection2 == "") {
+      setSelection2(event.target.value);
+    } else if (selection3 == "") {
+      setSelection3(event.target.value);
+    } else {
+      setSelection4(event.target.value);
+    }
   };
+  selections.push(selection1, selection2, selection3, selection4);
+  console.log(selections);
 
   const checkBoxInfo = [];
   for (let i = 1; i < 145; i++) {
@@ -30,6 +47,7 @@ const CheckBox = (props) => {
       <div
         key={data}
         className="col btn-group"
+        id="boxes"
         role="group"
         aria-label="Basic checkbox toggle button group"
       >
