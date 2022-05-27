e7ae9dd7b7a318024d4f9dbcab06c5f58f044a1f
vernellmiller <nezeey@gmail.com>
2022-05-27 00:30:59 -0400

    corrects checkbox selections

--- src/Components/CheckBox.js
+++ src/Components/CheckBox.js
@@ -1,48 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+const max = 4;
+// const checkIfWinner = function (arr1, arr2) {
+//   arr1.sort();
+//   arr2.sort();
 
+//   let str1 = arr1.join("");
+//   let str2 = arr2.join("");
+//   console.log(str1);
+//   console.log(str2);
+
+//   result =
+//     str1 == str2
+//       ? "You are a winner Today"
+//       : "Your selections do not match today. Try again Tomorrow.";
+
+//   console.log(result);
+//   return result;
+// };
 const CheckBox = (props) => {
-  // const [selections, setSelections] = useState({
-  //   selections: [],
-  // });
+  const [selections, setSelections] = useState([]);
 
-  const [selection1, setSelection1] = useState("");
-  const [selection2, setSelection2] = useState("");
-  const [selection3, setSelection3] = useState("");
-  const [selection4, setSelection4] = useState("");
+  // const [selection1, setSelection1] = useState("");
+  // const [selection2, setSelection2] = useState("");
+  // const [selection3, setSelection3] = useState("");
+  // const [selection4, setSelection4] = useState("");
 
-  const selections = [];
+  // const selections = [];
 
   const getSelectionHandler = (event) => {
-    const value =
-      event.target.type === "checkbox"
-        ? event.target.checked
-        : event.target.value;
-    console.log(event.target.value, value);
-    // setSelections((prevState) => {
-    //   return { ...prevState, selections: event.target.value };
-    // });
+    let newSelections = [...selections];
 
-    if (selection1 === "") {
-      setSelection1(event.target.value);
-    } else if (selection2 === "") {
-      setSelection2(event.target.value);
-    } else if (selection3 === "") {
-      setSelection3(event.target.value);
-    } else {
-      setSelection4(event.target.value);
+    if (newSelections.some((item) => item === event.target.value)) {
+      newSelections = newSelections.filter(
+        (item) => item !== event.target.value
+      );
+    } else if (newSelections.length < max) {
+      newSelections.push(event.target.value);
+      // checkIfWinner(newSelections, winning);
     }
+    setSelections(newSelections);
   };
-  selections.push(selection1, selection2, selection3, selection4);
-  console.log(selections);
 
   const checkBoxInfo = [];
   for (let i = 1; i < 145; i++) {
     checkBoxInfo.push(i);
   }
   // console.log(checkBoxInfo);
-
+  console.log("I was rendered!");
   const renderCheckBox = (data, index) => {
     const counter = `btncheck${data}`;
+    console.log(
+      selections.some((value) => value == data),
+      data,
+      selections
+    );
     return (
       <div
         key={data}
@@ -55,6 +66,7 @@ const CheckBox = (props) => {
           type="checkbox"
           className="checkBoxes btn-check"
           id={counter}
+          checked={selections.some((value) => value == data)}
           value={data}
           onChange={getSelectionHandler}
         />
